39f3dbbf2918af4dcf60e7543e31cb2bf78bb806
mshpo <nickshpo@gmail.com>
2022-07-03 19:48:07 -0400

    Complete the project

--- scripts.js
+++ scripts.js
@@ -1,19 +1,37 @@
-function computerPlay() {
-    const options = ['Rock', 'Paper', 'Scissors'];
-    let rand = Math.floor(Math.random() * 3); // 0, 1 or 2
-    return options[rand];
+const message = document.createElement('p');
+message.setAttribute('style', 'font-size: 3em; margin: 1em 0em; text-align: center');
+
+const score = document.createElement('p');
+let pWins = 0;
+let cWins = 0;
+score.textContent = `${pWins} : ${cWins}`;
+score.setAttribute('style', 'font-size: 5em; text-align: center; margin: 0px');
+
+const div = document.createElement('div');
+div.setAttribute('style', 'display: flex; flex-direction: column');
+div.appendChild(message);
+div.appendChild(score);
+
+const body = document.querySelector('body');
+body.appendChild(div);
+
+const buttons = document.querySelectorAll('button');
+
+function addResetButton() {
+    const resetButton = document.createElement('button');
+    resetButton.classList.add('resetButton');
+    resetButton.textContent = 'Play again?';
+    message.appendChild(resetButton);
+    resetButton.addEventListener('click', startGame);
 }
 
+function stopGame() {
+    buttons.forEach((button) => button.removeEventListener('click', eventHandler));
+}
 
-// Return 0 if a tie, 1 if player wins, and 2 if computer wins.
 function playRound(pSelection, cSelection) {
     let winMessage;
 
-    if (pWins >= 5 || cWins >= 5) {
-        pWins = 0;
-        cWins = 0;
-    }
-    
     if (pSelection === cSelection) {
         winMessage = "It's a tie. Play again.";
     } else if ( (pSelection === 'Rock' && cSelection === 'Scissors') ||
@@ -26,62 +44,44 @@ function playRound(pSelection, cSelection) {
         cWins++;
     }
 
-    if (pWins === 5)
+    if (pWins === 5) {
         message.textContent = 'YOU WON!!!';
-    else if (cWins === 5)
-        message.textContent = 'YOU LOST! =(';
-    else
+        message.style.color = 'red';
+        stopGame();
+        addResetButton();
+    } else if (cWins === 5) {
+        message.textContent = 'YOU LOST =(';
+        message.style.color = 'brown';
+        stopGame();
+        addResetButton();
+    } else
         message.textContent = winMessage;
 
     score.textContent = `${pWins} : ${cWins}`;
 }
 
-const buttons = document.querySelectorAll('button');
-const body = document.querySelector('body');
-const div = document.createElement('div');
-body.appendChild(div);
-
-const score = document.createElement('p');
-const message = document.createElement('p');
-div.appendChild(message);
-div.appendChild(score);
-
-let playerSelection;
-let pWins = 0;
-let cWins = 0;
-score.textContent = `${pWins} : ${cWins}`;
-
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        playerSelection = button.textContent;
-        playRound(playerSelection, computerPlay());
-    });
-});
-
-function game(numOfRounds) {
-
-    let pWins = 0;
-    let cWins = 0;
-
-    for (let i = 0; i < numOfRounds; i++) {
-        let computerSelection = computerPlay();
-        let response = playRound(playerSelection, computerSelection);
-
-        if (response === 1)
-            pWins++;
-        else if (response === 2)
-            cWins++;
-    }
-
-    // if (pWins > cWins)
-    //     p.textContent = 'YOU WON!';
-    // else if (pWins < cWins)
-    //     p.textContent = 'YOU LOST!';
-    // else
-    //     p.textContent = 'IT\'S A TIE';
+function computerPlay() {
+    const options = ['Rock', 'Paper', 'Scissors'];
+    let rand = Math.floor(Math.random() * 3); // 0, 1 or 2
+    return options[rand];
 }
 
+const eventHandler = (e) => {
+    let playerSelection = e.target.textContent;
+    playRound(playerSelection, computerPlay());
+};
 
+function startGame() {
+    message.textContent = 'Make your choice';
+    message.style.color = 'black';
+    buttons.forEach((button) => {
+            button.addEventListener('click', eventHandler);
+    });
 
+    pWins = 0;
+    cWins = 0;
+    score.textContent = `${pWins} : ${cWins}`;
+}
 
+startGame();
 
